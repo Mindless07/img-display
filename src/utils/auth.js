b85bf01760b7ef646910cc6a476ad26d5d24b417
Zizoune Nizar <zizoune.nizar@gmail.com>
2023-12-06 23:33:23 +0100

    initializing user with null

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,7 +5,7 @@ const CURRENT_USER_KEY = "CURRENT_USER_KEY";
 const currentUser = localStorage.getItem(CURRENT_USER_KEY);
 
 export const useAuth = () => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   useEffect(() => {
     getUser(currentUser).then((user) => setUser(user));
   }, []);
@@ -17,8 +17,10 @@ export const login = (username, password) => {
   if (
     (username === "muser1" && password === "mpassword1") ||
     (username === "muser2" && password === "mpassword2")
-  )
+  ) {
+    localStorage.clear();
     return localStorage.setItem(CURRENT_USER_KEY, username);
+  }
 
   if (username === "muser3" && password === "mpassword3")
     throw new Error("Blocked account");
